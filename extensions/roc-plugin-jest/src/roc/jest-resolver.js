2226365a826989d1b4c89aa4bc86f9ed87bdacf0
Andreas Søvik <arsovik@gmail.com>
2016-10-23 19:02:29 +0200

    Use invokeHook from util

--- extensions/roc-plugin-jest/src/roc/jest-resolver.js
+++ extensions/roc-plugin-jest/src/roc/jest-resolver.js
@@ -1,7 +1,7 @@
-const runHook = require('roc').runHook;
+import { invokeHook } from './util';
 const babelJest = require('babel-jest');
 
-const babelConfig = runHook('roc-plugin-jest')('babel-config', 'node');
+const babelConfig = invokeHook('babel-config', 'node');
 const babelJestTransformer = babelJest.createTransformer(babelConfig);
 
 module.exports = babelJestTransformer;
